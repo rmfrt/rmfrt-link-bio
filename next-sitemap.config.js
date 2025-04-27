8c9c3903d1ef0a8fee5db97223acf2ed3cc7bed6
Rémi Forte <forte.remi@gmail.com>
2025-04-27 20:47:07 +0200

    Add next-sitemap for sitemap generation and update package dependencies

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: "https://rmfrt.com", // Remplacez par l'URL de votre site
+    generateRobotsTxt: true, // Génère également un fichier robots.txt
+    sitemapSize: 7000, // Optionnel : limite le nombre d'URLs par fichier sitemap
+};
