362394bfc7f9d5b52190027e219e97886e64d51a
Rémi Forte <forte.remi@gmail.com>
2025-04-27 20:29:09 +0200

    Fix metadata formatting and add Google Tag Manager to RootLayout component

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,13 +1,17 @@
 import "./globals.css";
+import { GoogleTagManager } from "@next/third-parties/google";
 import Header from "./components/Header";
 
 export const metadata = {
-    title: "Rémi Forte ",
+    title: "Rémi Forte",
+    description: "Poetic Program, Typographic System",
+    keywords: "poetry, typography, design, graphic design, art, code",
 };
 
 export default function RootLayout({ children }) {
     return (
         <html lang="en">
+            <GoogleTagManager gtmId="GTM-T3CWJ57W" />
             <body className="py-12">
                 <Header />
                 <div className="">{children}</div>
